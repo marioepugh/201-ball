31b5fff4d810a11eb83520269f60735776efffc7
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-01 16:32:19 -0700

    add a shaky shaky

---
+++ js/ballScript.js
@@ -0,0 +1,17 @@
+var ballContainer = document.getElementById('ballOuter');
+var ballContent = document.getElementById('ballContent');
+
+var answers = ball.answers.html;
+
+var randomNumber = function(num) {
+  return Math.floor(Math.random() * num);
+};
+
+
+var changeQuote = function() {
+  ballContent.textContent = answers[randomNumber(answers.length)];
+}
+
+changeQuote();
+
+ballContainer.addEventListener('click', changeQuote());
