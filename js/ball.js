856a5015dd1420661ba0ff83ec0d771c0580a348
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-02 16:11:24 -0700

    pulling stuff from local storage

--- js/ball.js
+++ js/ball.js
@@ -50,7 +50,7 @@ var ball = {
       'Fixed positioning stays in place when page is scrolled',
       'CSS font-size: the text size to be used'
     ],
-    js: [
+    javascript: [
       'Are you calling your variables',
       'Check your spelling',
       'JavaScript programs the behavior of web pages',
@@ -116,6 +116,7 @@ var ball = {
       'Outlook not so good',
       'Very doubtful'
     ]
-
   }
 };
+
+ball;
