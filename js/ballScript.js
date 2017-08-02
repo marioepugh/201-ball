32af17f3dd37a0dfb4ffdbf4d6ff836893321f70
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-02 10:05:54 -0700

    providing multiple arrays to the ball

--- js/ballScript.js
+++ js/ballScript.js
@@ -3,53 +3,27 @@
 var ballContainer = document.getElementById('ballOuter');
 var ballContent = document.getElementById('ballContent');
 
-
 var answers = ball.answers;
-var picked = ['html', 'css', 'js'];
-
-var arr = [];
+var picked = ['traditional'];
+var filtered = [];
 
 picked.forEach(function(item) {
-  arr.push(answers[item]);
+  filtered.push(answers[item]);
 });
 
-var combined = arr.reduce(function(a, b) {
+var conbinedAnswers = filtered.reduce(function(a, b) {
   return a.concat(b);
 }, []);
 
-console.log(combined);
-
-// console.log(answersFiltered);
-
-
-// picked.forEach(function(item) {
-//   // console.log(item);
-//   for (var key in answers) {
-//     // console.log(key);
-//     if (key === item){
-//       // arr.push(answers[key]);
-//       console.log(key)
-//     }
-//   }
-// });
-//
-
-
-
-
-
-console.log(answers);
+console.log(conbinedAnswers);
 
 var randomNumber = function(num) {
   return Math.floor(Math.random() * num);
 };
 
-
 var changeQuote = function() {
-  ballContent.textContent = answers[randomNumber(answers.length)];
+  ballContent.textContent = conbinedAnswers[randomNumber(conbinedAnswers.length)];
 };
-
-
 
 changeQuote();
 
