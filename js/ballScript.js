36d3fb0d4a02877d50d9e36f25286889e5c39d95
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-02 09:50:56 -0700

    array combined

--- js/ballScript.js
+++ js/ballScript.js
@@ -4,7 +4,41 @@ var ballContainer = document.getElementById('ballOuter');
 var ballContent = document.getElementById('ballContent');
 
 
-var answers = ball.answers.html;
+var answers = ball.answers;
+var picked = ['html', 'css', 'js'];
+
+var arr = [];
+
+picked.forEach(function(item) {
+  arr.push(answers[item]);
+});
+
+var combined = arr.reduce(function(a, b) {
+  return a.concat(b);
+}, []);
+
+console.log(combined);
+
+// console.log(answersFiltered);
+
+
+// picked.forEach(function(item) {
+//   // console.log(item);
+//   for (var key in answers) {
+//     // console.log(key);
+//     if (key === item){
+//       // arr.push(answers[key]);
+//       console.log(key)
+//     }
+//   }
+// });
+//
+
+
+
+
+
+console.log(answers);
 
 var randomNumber = function(num) {
   return Math.floor(Math.random() * num);
@@ -13,8 +47,9 @@ var randomNumber = function(num) {
 
 var changeQuote = function() {
   ballContent.textContent = answers[randomNumber(answers.length)];
-
 };
+
+
 
 changeQuote();
 
